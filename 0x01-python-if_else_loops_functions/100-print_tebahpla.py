c0597e928ccaae4346660ce4e1af9bf253dce460
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-10-31 14:54:45 -0700

    One bon

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 low = list(range(122, 96, -2))
 up = list(range(89, 64, -2))
 for c_low, c_up in zip(low, up):
-    print(f"{c_low:c}{c_up:c}", end="")
+    print("{0:c}{1:c}".format(c_low, c_up), end="")
