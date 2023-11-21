22e890076e0f9d7c68ac817384b294b819d83180
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-21 14:45:30 -0800

    Ex 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is a square class"""
+
+
+class Square:
+    """ This is an empty class.
+
+    Trying multiple ones
+    """
+    def __init__(self, size):
+        """ Instantiate the instance with size """
+        self.__size = size
