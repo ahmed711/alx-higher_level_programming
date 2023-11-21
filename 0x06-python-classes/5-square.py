779dd24c5a07db3e0ad71fe7cd85ebec9659455e
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-21 15:21:51 -0800

    Ex 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" This is a square class"""
+
+
+class Square:
+    """ This is an empty class.
+
+    Trying multiple ones
+    """
+    def __init__(self, size=0):
+        """ Instantiate the instance with size """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.size = size
+
+    def area(self):
+        "Return the area of square"
+        return self._Square__size ** 2
+
+    @property
+    def size(self):
+        """get size property"""
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """ Retrieve the size of a square and set it when value is provided"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+
+    def my_print(self):
+        if not self.size:
+            print()
+        else:
+            [print("#" * self.size) for i in range(self.size)]
