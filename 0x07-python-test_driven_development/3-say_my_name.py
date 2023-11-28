b263a2fa30317bf591d565194689a749efaba067
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-28 14:14:15 -0800

    Ex 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+say my name function
+
+prints first and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ print first and last name of a person"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
