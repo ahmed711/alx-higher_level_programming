1fc1b35868eabc3dc87e83326d2c2e8408a4ab3b
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-28 06:30:21 -0800

    Ex 31

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+class with only first_name attribute
+
+no other attributes are allowed
+"""
+
+
+class LockedClass:
+    """ empty class """
+    __slots__ = "first_name"
