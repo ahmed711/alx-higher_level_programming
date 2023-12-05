2bb70b7c78e014e109ce85caf36a64b13ba222eb
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-12-04 16:04:23 -0800

    Ex 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" a class that inherits from list """
+
+
+class MyList(list):
+    """ a class that inherits from list """
+    def print_sorted(self):
+        print(sorted(self))
