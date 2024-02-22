142bcdc5f1d5d9e187769c3d9124e93aa080d26f
ahmed711 <ahmed_tayseer12@yahoo.com>
2024-02-21 17:17:24 -0800

    Ex 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints the string 'Javascript is amazing'
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}
