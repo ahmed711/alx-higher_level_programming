81491b3e54a473ab11d692b2ce1639fe1ced7ca7
ahmed711 <ahmed_tayseer12@yahoo.com>
2024-06-18 16:08:48 -0700

    12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints the string 'Javascript is amazing'
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
